28bd4165536f6f18fcaa19c359abec480b94cd84
unknown <juniorthx@gmail.com>
2021-09-24 12:48:16 -0700

    start config to deploy

--- rest-api-photos/src/app.js
+++ rest-api-photos/src/app.js
@@ -1,11 +1,15 @@
-const express=require("express")
-const app=express();
+const express=require("express");
 const cors=require("cors");
 const bodyParser=require("body-parser");
-const pictureRoutes=require("./routes/pictures");
-require("./models/dbConfig");
+const pictureRoutes=require("../routes/pictures");
 const morgan=require("morgan");
+const serverless=require("serverless-http");
+const router=express.Router();
+require("../models/dbConfig");
+
 const PORT=process.env.PORT || 4000;
+
+const app=express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -13,31 +17,25 @@ app.use(cors())
 app.use("/pictures", pictureRoutes); 
 app.use(morgan('dev'));
 
-app.get("/", (req, res)=>{
+router.get("/", (req, res)=>{
     res.json({"message":"API to get all uploaded images. Add /pictures to the URL to see all informations about uploading images."})
-})
+});
 
-app.use((req, res, next)=>{
-  res.header('Access-Control-Allow-Origin','*');
-  res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Contrnt-Type, Accept, Authorization');
-  if(req.method === 'OPTIONS'){
-    req.header('Access-Control-Allow-Methods',"PUT, POST, PATCH, DELETE, GET") ;
-    return res.status(200).json({})
-  }
-})
-
+app.use('/.netlify/functions/app', router);
 app.use((req, res, next)=>{
     const error=new Error('Page Not found');
     error.status=404;
     next(error);
 })
+
 app.use((error, req, res, next)=>{
     res.status(error.status || 500);
     res.json({message:error.message})
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Server started on localhost port ${PORT}`)
-});
+// app.listen(PORT, ()=>{
+//     console.log(`Server started on localhost port ${PORT}`)
+// });
 
 module.exports=app;
+module.exports.handler=serverless(app);
