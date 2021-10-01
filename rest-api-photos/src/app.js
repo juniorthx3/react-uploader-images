ece59c232ef44c1a1c12b1907f3f75f97f9a4e67
unknown <juniorthx@gmail.com>
2021-10-01 13:10:22 -0700

    deployed successfully

--- rest-api-photos/src/app.js
+++ rest-api-photos/src/app.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const cors=require("cors");
 const pictureRoutes=require("../routes/pictures");
-// const morgan=require("morgan");
+const morgan=require("morgan");
 require("../models/dbConfig");
 const serverless=require("serverless-http");
 
@@ -11,7 +11,7 @@ const PORT=process.env.PORT || 4000;
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors())
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 const router=express.Router();
 
 router.get("/", (req, res)=>{
@@ -21,22 +21,21 @@ app.use('/.netlify/functions/app', router);
 app.use("/.netlify/functions/app/pictures", pictureRoutes); 
 
 
-// app.use((req, res, next)=>{
-//     const error=new Error('Page Not found...');
-//     error.status=404;
-//     next(error);
-// })
-// app.use((error, req, res, next)=>{
-//     res.status(error.status || 500);
-//     res.json({message:error.message})
-// })
+app.use((req, res, next)=>{
+    const error=new Error('Page Not found...');
+    error.status=404;
+    next(error);
+})
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    res.json({message:error.message})
+})
 
 
-// app.listen(PORT, ()=>{
-//     console.log(`Server started on localhost port ${PORT}`)
-// });
+app.listen(PORT, ()=>{
+    console.log(`Server started on localhost port ${PORT}`)
+});
 
-// module.exports=app;
-
+module.exports=app;
 module.exports.handler=serverless(app);
 
