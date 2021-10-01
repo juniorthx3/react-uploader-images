c8691866ff056e2758988d9b33249c2000fdc6d8
unknown <juniorthx@gmail.com>
2021-10-01 12:47:03 -0700

    trying to host on netlify

---
+++ rest-api-photos/src/app.js
@@ -0,0 +1,42 @@
+const express=require("express");
+const cors=require("cors");
+const pictureRoutes=require("../routes/pictures");
+// const morgan=require("morgan");
+require("../models/dbConfig");
+const serverless=require("serverless-http");
+
+const app=express();
+const PORT=process.env.PORT || 4000;
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors())
+// app.use(morgan('dev'));
+const router=express.Router();
+
+router.get("/", (req, res)=>{
+    res.json({"message":"API to get all uploaded images. Add /pictures to the URL to see all informations about uploading images."})
+});
+app.use('/.netlify/functions/app', router);
+app.use("/pictures", pictureRoutes); 
+
+
+// app.use((req, res, next)=>{
+//     const error=new Error('Page Not found...');
+//     error.status=404;
+//     next(error);
+// })
+// app.use((error, req, res, next)=>{
+//     res.status(error.status || 500);
+//     res.json({message:error.message})
+// })
+
+
+// app.listen(PORT, ()=>{
+//     console.log(`Server started on localhost port ${PORT}`)
+// });
+
+// module.exports=app;
+
+module.exports.handler=serverless(app);
+
