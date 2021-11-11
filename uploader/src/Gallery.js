07899038c92a5a90826656a910324e6e0122fe8f
unknown <juniorthx@gmail.com>
2021-11-11 09:07:08 -0800

    Update frontend

--- uploader/src/Gallery.js
+++ uploader/src/Gallery.js
@@ -26,12 +26,12 @@ const Gallery = ({title, selectedImage}) => {
       <React.Fragment>
             <h1>{title}</h1>
             <div className="gallery">
-                <img className="gallery-item" alt="" src={`http://localhost:4000/photo/display/2021-11-05T19-09-32.944Z-cat1.jpg`} />
-                <img className="gallery-item" alt="" src={`http://localhost:4000/photo/display/2021-11-05T19-09-37.182Z-cat2.jpg`} />
-                <img className="gallery-item" alt="" src={`http://localhost:4000/photo/display/2021-11-05T19-09-40.985Z-cat3.jpg`} />
-                <img className="gallery-item" alt="" src={`http://localhost:4000/photo/display/2021-11-05T19-09-45.564Z-cat4.jpg`} />
-                <img className="gallery-item" alt="" src={`http://localhost:4000/photo/display/2021-11-05T19-09-49.714Z-cat5.jpg`} />
-                <img className="gallery-item" alt="" src={`http://localhost:4000/photo/display/2021-11-05T19-09-55.255Z-cat6.jpg`} />
+                <img className="gallery-item" alt="" src={`http://localhost:4000/photo/display/name/2021-11-05T19-09-32.944Z-cat1.jpg`} />
+                <img className="gallery-item" alt="" src={`http://localhost:4000/photo/display/name/2021-11-05T19-09-37.182Z-cat2.jpg`} />
+                <img className="gallery-item" alt="" src={`http://localhost:4000/photo/display/name/2021-11-05T19-09-40.985Z-cat3.jpg`} />
+                <img className="gallery-item" alt="" src={`http://localhost:4000/photo/display/name/2021-11-05T19-09-45.564Z-cat4.jpg`} />
+                <img className="gallery-item" alt="" src={`http://localhost:4000/photo/display/name/2021-11-05T19-09-49.714Z-cat5.jpg`} />
+                <img className="gallery-item" alt="" src={`http://localhost:4000/photo/display/name/2021-11-05T19-09-55.255Z-cat6.jpg`} />
                 {/* {display(image)} */}
            </div>
       </React.Fragment>
